544866d53988df8863926010bc67fd9119cda5f3
Stanly Johnson <stanlyjohnson@outlook.com>
2018-05-22 20:24:40 +0530

    contract-added

---
+++ contracts/NullToken.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.2;
+
+contract NullToken {
+
+  uint256 public totalSupply;
+
+  function NullToken() public {
+      totalSupply = 1000;
+
+  }
+}
--- truffle.js
+++ truffle.js
@@ -1,4 +1,13 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks : {
+    development : {
+      host : "127.0.0.1",
+      //port number for ganache
+      port : "7545",
+      //let all network_id to connect
+      network_id : "*",
+    }
+  }
 };
