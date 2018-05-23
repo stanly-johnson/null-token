f6aa4650f3dfe63396876d317ccf95712fc16f37
Stanly Johnson <stanlyjohnson@outlook.com>
2018-05-23 20:05:22 +0530

    tranfer-added

--- contracts/NullToken.sol
+++ contracts/NullToken.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.4.2;
 contract NullToken {
 
   uint256 public totalSupply;
+  string constant public name = 'NullToken';
+  string constant public symbol = 'NUL';
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
   mapping(address => uint256) public balanceOf;
 
@@ -10,4 +14,12 @@ contract NullToken {
       totalSupply = _seedAmount;
       balanceOf[msg.sender] = totalSupply;
   }
-}
+
+  function transfer(address _toAddress, uint256 _value) public returns (bool success){
+    //check if the sender has tokens > sending value
+      require(balanceOf[msg.sender] >= _value);
+      balanceOf[msg.sender] -= _value;
+      balanceOf[_toAddress] += _value;
+  }
+
+}//end of contract
--- test/NullToken.js
+++ test/NullToken.js
@@ -2,9 +2,26 @@ var NullToken = artifacts.require("./NullToken.sol");
 
 contract('NullToken', function(accounts) {
 
-  //change the total supply value here
+  //specify the smart contract test values here
   const $totalCount = 1000;
   const $ownerAddress = accounts[0];
+  const $tokenName = 'NullToken';
+  const $tokenSymbol = 'NUL';
+  const $recipentAddress = accounts[2];
+  const $transferTokenTestNumber = 10;
+
+  //test for contract initialisation
+  it('Test for Contract values', function(){
+    return NullToken.deployed().then(function(instance){
+        nullTokenInstance = instance;
+        return nullTokenInstance.name();
+    }).then(function(_tokenName){
+      assert.equal(_tokenName, $tokenName, 'correct name has been assigned to token');
+      return nullTokenInstance.symbol();
+    }).then(function(_tokenSymbol){
+      assert.equal(_tokenSymbol, $tokenSymbol, 'correct token symbol has been assigned');
+    })
+  });
 
   //check if the total supply of the token is as intended
   it('Test for total supply', function() {
@@ -23,6 +40,24 @@ contract('NullToken', function(accounts) {
       return nullTokenInstance.balanceOf($ownerAddress);
     }).then(function(owner_balance){
       assert.equal(owner_balance.toNumber(), $totalCount, 'set to owner balance to total supply');
+    })
+  });
+
+  //check if token transfer happens correctly
+  it('Test for token transfer', function(){
+    return NullToken.deployed().then(function(instance){
+      nullTokenInstance = instance;
+      return nullTokenInstance.transfer.call($ownerAddress, $totalCount+100);
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.indexOf('revert') >= 0, 'error message must have revert');
+      return nullTokenInstance.transfer($recipentAddress, $transferTokenTestNumber, { from : $ownerAddress});
+    }).then(function(receipt){
+      return nullTokenInstance.balanceOf($recipentAddress);
+    }).then(function(recipentBalance){
+      assert.equal(recipentBalance.toNumber(), $transferTokenTestNumber, 'add the value to recipent account');
+      return nullTokenInstance.balanceOf($ownerAddress);
+    }).then(function(ownerBalance){
+      assert.equal(ownerBalance.toNumber(), $totalCount-$transferTokenTestNumber, 'deduct the value from the sender account');
     })
   });
 
