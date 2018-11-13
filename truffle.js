ffd990dec573148e06207a0c1b8f76c2f98a47ea
Stanly Johnson <stanlyjohnson@outlook.com>
2018-11-13 09:44:27 +0700

    rinkeby added

--- truffle.js
+++ truffle.js
@@ -8,6 +8,12 @@ module.exports = {
       port : "7545",
       //let all network_id to connect
       network_id : "*",
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4700000
     }
   }
 };
